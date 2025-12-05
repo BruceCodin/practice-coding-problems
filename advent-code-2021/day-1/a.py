cbaa04bb0dcf51012d6d6fe0cec49a500792af30
BruceCodin <127963534+BruceCodin@users.noreply.github.com>
2025-12-05 16:21:03 +0000

    feat: implemented initial files for day 1 sonar sweep part 1

---
+++ advent-code-2021/day-1/a.py
@@ -0,0 +1,16 @@
+"""
+Python code solves advent-code-2021: day 1 sonor sweep (part 1)
+"""
+
+def read_input(filename: str) -> list[str]:
+    """ Read input txt file and return list of str inputs """
+    with open(filename, 'r') as f:
+        return f.readlines()
+    
+def count_increment(list_input:list[str]) -> int: 
+    """ Count how many times a depth measurement increases from the previous one. """
+    pass 
+
+
+if __name__ == "__main__":
+    pass 
