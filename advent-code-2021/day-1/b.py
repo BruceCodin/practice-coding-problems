d3e2db7bb5133d8cd5bb2f1842734ed0e3594d03
BruceCodin <127963534+BruceCodin@users.noreply.github.com>
2025-12-06 19:20:58 +0000

    feat: initial files for part 2 set up

---
+++ advent-code-2021/day-1/b.py
@@ -0,0 +1,24 @@
+"""
+Python code solves advent-code-2021: day 1 sonor sweep (part 2)
+https://adventofcode.com/2021/day/1
+"""
+
+def read_input(filename: str) -> list[str]:
+    """ Read input txt file and return list of str inputs """
+    with open(filename, 'r') as f:
+        return f.readlines()
+
+def convert_list_str(input_lines: list[str]) -> list[int]:
+    """ Converts list of str to list of int """
+    pass 
+
+def count_increment(sliding_windows :list[tuple]) -> int:
+    """ Returns count of how many times each subsequent sliding window sum increased from previous """
+    pass 
+
+def sliding_measurement(input_lines: list[str]) -> int:
+    """ Get three measurement sliding window sums and how many times they increase """
+    pass 
+
+if __name__ == "__main__":
+    pass 
---
+++ advent-code-2021/day-1/test_b.py
@@ -0,0 +1,17 @@
+""" pytest test file for day-1: b """
+#pylint: skip-file
+
+from b import convert_list_str, count_increment, sliding_measurement
+import pytest 
+
+class TestConvertListStr:
+    """ Tests for convert_list_str """
+    pass 
+
+class TestCountIncrement:
+    """ Tests for count_increment """
+    pass 
+
+class TestSlidingMeasurement:
+    """ Tests for sliding_measurement """
+    pass 
