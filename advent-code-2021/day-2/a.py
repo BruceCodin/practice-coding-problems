0221213c8d2cfb1d051f5001892eb2596be2f312
BruceCodin <127963534+BruceCodin@users.noreply.github.com>
2025-12-08 13:47:41 +0000

    feat: initial TDD

---
+++ advent-code-2021/day-2/a.py
@@ -0,0 +1,24 @@
+"""
+Python code solves advent-code-2021: Day 2: Dive! (part 1)
+https://adventofcode.com/2021/day/2
+"""
+
+def read_input(filename: str) -> list[str]:
+    """Read input txt file and return list of str inputs"""
+    with open(filename, 'r') as f:
+        return f.readlines()
+
+def parse_command(line:str) -> list:
+    """Parse each line and separate into direction and unit"""
+    pass 
+
+def get_horizontal_depth(readlines_input: list[str]) -> tuple:
+    """Get the total horziontal and depth values"""
+    pass 
+
+def calculate_product(horizontal:int, depth:int) -> int:
+    """Calculate final product"""
+    pass 
+
+if __name__ == "__main__":
+    pass
---
+++ advent-code-2021/day-2/test_a.py
@@ -0,0 +1,14 @@
+""" pytest test file for day-1: a """
+#pylint: skip-file
+
+from a import parse_command, get_horizontal_depth, calculate_product
+import pytest 
+
+class TestParseCommand:
+    pass 
+
+class TestGetHorizontalDepth:
+    pass 
+
+class TestCalculateProduct: 
+    pass 
