43e23663cb086f01c09e0f89c9e243baf42641f9
BruceCodin <127963534+BruceCodin@users.noreply.github.com>
2025-12-16 16:36:20 +0000

    feat: initial day 6 files

---
+++ advent-code-2021/day-6/a.py
@@ -0,0 +1,4 @@
+"""
+Python code solves advent-code-2021: Day 6: Lanternfish (Part 1)
+https://adventofcode.com/2021/day/6
+"""
---
+++ advent-code-2021/day-6/test_a.py
@@ -0,0 +1,4 @@
+"""pytest test file for day-6: a"""
+# pylint: skip-file
+
+import pytest
